dfaa8ad9e39b9a9516e2aacd9e5356ab9b2e405c
WinnyTroy <kiraguwinnie@gmail.com>
2016-11-07 07:38:19 +0300

    upload of project setup, up until saving of the data in the db from the views

---
+++ ground_zero/temperature/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+
+from temperature import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^$', views.get_temperature, name='get_temp'),
+
+]
