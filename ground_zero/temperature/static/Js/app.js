4110dcbb32761fe2e4659e7b37aafd3ee529d19a
WinnyTroy <kiraguwinnie@gmail.com>
2016-11-14 09:54:33 +0300

    displayin of tempdata in the graph.

--- ground_zero/temperature/static/Js/app.js
+++ ground_zero/temperature/static/Js/app.js
@@ -1,66 +1,63 @@
+//$(function () {
+//    var myChart = Highcharts.chart('container', {
+//        chart: {
+//            type: 'spline'
+//        },
+//        title: {
+//            text: 'Weekly Temperature Evaluation'
+//        },
+//        xAxis: {
+//            type: 'datetime',
+//            title: {
+//                text: 'Date/Time'
+//            }
+//        },
+//        yAxis: {
+//            title: {
+//                text: 'Temp Value'
+//            }
+//        },
+//        series: [{  }]
+//
+//    });
+//});
+
+
 $(function () {
-    var myChart = Highcharts.chart('container', {
-        chart: {
-            type: 'column'
-        },
+    Highcharts.chart('container', {
         title: {
-            text: 'Weekly Temperature Evaluation'
-        },
+            text: 'Weekly Temperature Evaluation',
+            x: -20 //center
+        },  
         xAxis: {
-            type: 'datetime',
-            title: {
-                text: 'Date/Time'
-            }
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         },
         yAxis: {
             title: {
-                text: 'Temp Value'
-            }
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
         },
-        series: [{ }]
-
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Temp',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }]
     });
 });
 
 
-//$(function () {
-//    $.getJSON('http://127.0.0.1:8000/ground_zero/data/jsonp.php?filename=range.json&callback=?', function(data) {
-//
-//    	$('#container').highcharts({
-//
-//		    chart: {
-//		        type: 'spline'
-//		    },
-//
-//		    title: {
-//		        text: 'Temperature variation by day'
-//		    },
-//
-//		    xAxis: {
-//		        type: 'datetime'
-//		    },
-//
-//		    yAxis: {
-//		        title: {
-//		            text: null
-//		        }
-//		    },
-//
-//		    tooltip: {
-//		        crosshairs: true,
-//		        shared: true,
-//		        valueSuffix: '°C'
-//		    },
-//
-//		    legend: {
-//		        enabled: false
-//		    },
-//
-//		    series: [{
-//
-//		    }]
-//
-//		});
-//    });
-//
-//});
+
